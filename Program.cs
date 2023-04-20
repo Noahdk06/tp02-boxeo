f7675491cadb098f574e3d337f2f504c0a537a45
NOAH DENENBERG KOROB <49262208@ort>
2023-04-20 14:43:55 -0300

    create new console project

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿
+
+   
+
+    
+       double skill1=0;
+       double skill2=0;
+        
+            
+           int opcion = menu();
+            while (opcion != 4)
+            {
+
+                switch (opcion)
+                {
+
+                    case 1:
+                          Boxeador boxeador1 =  ingresarBoxeador();
+                           skill1= boxeador1.obtenerSkill(boxeador1.VelPiernas,boxeador1.PotGolpes);
+                        break;
+                    case 2:
+                          Boxeador boxeador2 =  ingresarBoxeador();
+                           skill2= boxeador2.obtenerSkill(boxeador2.VelPiernas,boxeador2.PotGolpes);
+                        break;
+                    case 3:
+                         
+                         string finP = pelea(boxeador1,boxeador2, skill1, skill2);
+                        break;
+                    
+                }
+
+
+
+
+
+
+
+                opcion = menu();
+
+            
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Boxeador ingresarBoxeador()
+            {   
+                
+                string nom;
+            string pais;
+             int peso; 
+             int potencia;
+              int velocidad;
+             Console.WriteLine("Ingrese el nombre del boxeador");
+             nom = Console.ReadLine();
+             
+             System.Console.WriteLine("ingrese la nacionalidad del boxeador");
+             pais = Console.ReadLine();
+             System.Console.WriteLine("ingrese el peso del boxeador");
+             peso = int.Parse(Console.ReadLine());
+             System.Console.WriteLine("ingrese la potencia del golpeo del boxeador");
+             potencia = int.Parse(Console.ReadLine());
+             System.Console.WriteLine("ingrese la velocidad de las piernas del boxeador");
+             velocidad = int.Parse(Console.ReadLine());
+
+             Boxeador unBoxeador = new Boxeador(nom, pais, peso, potencia, velocidad);
+         
+             return unBoxeador;
+
+            }
+
+
+         string pelea(Boxeador box1, Boxeador box2, double skl1,double skl2)
+         {
+            string mensaje="";
+            string mensajeGanador="";
+            Boxeador boxeadorGanador;
+          System.Console.WriteLine("el boxeador1 tiene " + skl1 + " y se enfrentara al boxeador numero 2, que tiene " + skl2 );
+           
+           if (skl1>skl2)
+           {
+            boxeadorGanador=box1;
+           }
+           else
+           {
+            boxeadorGanador=box2;
+           }
+           if(skl1 -skl2 >= 30 || skl1 - skl2 >= -30)
+           {
+             mensaje="se gano por ko";
+           }
+           if(skl1 - skl2 >= 10 && skl1 - skl2 < 30 || skl1 - skl2 >= -10 && skl1 - skl2 < -30){
+            mensaje = "Por puntos en fallo unanime";
+           }
+           if(skl1 - skl2 <= 10  || skl1 - skl2 <= -10){
+            mensaje = "Por puntos en fallo dividido";
+           }
+
+           mensajeGanador= "el ganador es el " + boxeadorGanador + mensaje;
+           
+         return mensajeGanador;
+         }
+
+
+
+
+
+
+
+
+        
+
+
+            
+                
+               int menu()
+               {
+                int opElegida;
+                Console.Clear();
+                System.Console.WriteLine("///////////////////menu/////////////////////");
+                System.Console.WriteLine("i-Cargar Datos Boxeador 1");
+                System.Console.WriteLine("ii-Cargar Datos Boxeador 2");
+                System.Console.WriteLine("iii-Pelear!");
+                System.Console.WriteLine("iv-Salir");
+                opElegida = int.Parse(System.Console.ReadLine());
+
+                while (opElegida < 0 || opElegida > 4)
+                {
+                    System.Console.WriteLine("Opcion invalida, intente ingresar un numero entre 1 y 4");
+                    opElegida = int.Parse(System.Console.ReadLine());
+                }
+
+
+
+                return opElegida;
+               } 
+                
+            
+
+
+
+
+
+
+
+    
+
+
---
+++ boxeador.cs
@@ -0,0 +1,47 @@
+using System;
+
+    class Boxeador
+    {
+
+        public string Nombre { get; set; }
+        public int Peso { get; private set; }
+        public int PotGolpes { get; set; }
+        public int VelPiernas { get; set; }
+        public string Pais { get; set; }
+
+        public Boxeador()
+        {
+         Nombre ="";
+         Pais = "";
+         Peso=0;
+         PotGolpes=0;
+         VelPiernas=0;
+        }
+        public Boxeador(string nom, string pais, int peso, int potencia, int velocidad)
+        {
+            Nombre = nom;
+            Peso = peso;
+            PotGolpes = potencia;
+            VelPiernas = velocidad;
+            Pais = pais;
+        }
+        
+
+        public double obtenerSkill(int velocidad, int potencia)
+        {
+           double skill=0;
+           int min = 1;
+           int max = 10;
+           
+        Random azar = new Random();
+        int num =azar.Next(min, max + 1);
+        
+        skill= (velocidad*0.6)+(potencia*0.8)+num;
+
+
+        return skill;
+        }
+
+        
+        
+    }
